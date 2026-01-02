15526d8e4cbf6ac9945b159bb46af5f469d6d717
Pierre-Richard BEBE <pierre.richard.prb@gmail.com>
2026-01-02 13:13:53 -0300

    up recipe symfony/stimulus-bundle

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
-import {registerReactControllerComponents} from '@symfony/ux-react';
 import './bootstrap.js';
+import {registerReactControllerComponents} from '@symfony/ux-react';
 /*
  * Welcome to your app's main JavaScript file!
  *
