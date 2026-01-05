4a8c734853ae591e4315bf248f4b0b08cd8f88a7
Pierre-Richard BEBE <pierre.richard.prb@gmail.com>
2026-01-05 15:53:37 -0300

    fix route (prev up recipe symfony/routing)

--- src/Controller/CollectionsController.php
+++ src/Controller/CollectionsController.php
@@ -10,9 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('collections', name: 'collections.')]
+#[Route('/collections', name: 'collections.')]
 class CollectionsController extends AbstractController
 {
 
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use App\Repository\BrandsRepository;
 use App\Repository\SneakerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
