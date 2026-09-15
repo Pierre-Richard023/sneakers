a27ac454509aafab195df6ceb54280ee12ad83e6
Pierre-Richard BEBE <pierre.richard.prb@gmail.com>
2026-09-15 20:00:38 -0300

    fix/security-dependencies part. 1

--- assets/app.js
+++ assets/app.js
@@ -10,6 +10,5 @@ import {registerReactControllerComponents} from '@symfony/ux-react';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-import 'flowbite';
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
